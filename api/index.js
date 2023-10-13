3f4402897fb5f344d6b93b2231b441cb49f77e4e
viram158 <viramjadhav158@gmail.com>
2023-10-13 15:25:06 +0530

    connected with server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 mongoose
-.connect("mongodb://localhost:27017/Mern-estate").then(()=>{
+.connect("mongodb://127.0.0.1:27017/Mern-estate").then(()=>{
     console.log("connected to mongodb")
 }).catch((err)=>{
 console.log(err)
