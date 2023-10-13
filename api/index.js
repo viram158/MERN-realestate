a32ee3803cf57306ae8fab342127ba3520047274
viram158 <viramjadhav158@gmail.com>
2023-10-13 15:18:21 +0530

    changes are happening

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+mongoose
+.connect("mongodb://localhost:27017/Mern-estate").then(()=>{
+    console.log("connected to mongodb")
+}).catch((err)=>{
+console.log(err)
+})
 const app = express();
 
 app.listen(3000,()=>{
